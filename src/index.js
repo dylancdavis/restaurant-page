0a49b817576ddeb70e816adeb46111763013ffa0
Cole D <dylcdav@gmail.com>
2023-01-26 10:01:28 -0500

    Integrate HTML Webpack Plugin

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import './style.css';
 import getComponents from './components.js'
 
+// Add content div to body
+let content = document.createElement('div');
+content.id = 'content'
+document.body.appendChild(content)
+
 const components = getComponents();
 console.log(components)
-
-const content = document.getElementById('content')
 
 
 for (let c in components) {
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     devtool: 'inline-source-map',
     entry: './src/index.js',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Nicola's"
+        })
+    ],
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
